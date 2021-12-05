9a54c84c20ce58ebd415a1bc65651d7e90474acb
pavel_gavrilichev <Pavel.Gavrilichev@waveaccess.ru>
2021-12-05 19:45:18 +0300

    add error handler

--- KFU.CinemaOnline.API/Startup.cs
+++ KFU.CinemaOnline.API/Startup.cs
@@ -1,21 +1,30 @@
 using System;
+using System.Net;
+using System.Threading.Tasks;
 using Autofac;
+using AutoMapper;
+using KFU.CinemaOnline.BL;
 using KFU.CinemaOnline.Common;
+using KFU.CinemaOnline.DAL;
 using KFU.CinemaOnline.DAL.Account;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Rest;
+using Newtonsoft.Json;
 
 namespace KFU.CinemaOnline.API
 {
     public class Startup
     {
-        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +35,11 @@ namespace KFU.CinemaOnline.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.AddProfile(typeof(ApiMapperProfile));
+                cfg.AddProfile(typeof(BlMapperProfile));
+            });
             services.AddControllers();
             services.Configure<AuthOptions>(Configuration.GetSection("Auth"));
 
@@ -48,16 +62,22 @@ namespace KFU.CinemaOnline.API
 
         public void ConfigureContainer(ContainerBuilder builder)
         {
-            //builder.RegisterModule<DataAccessDependencyModule>();
+            builder.RegisterModule<DataAccessDependencyModule>();
 
-            builder.RegisterModule<MapperDependencyModule>();
+            //builder.RegisterModule<MapperDependencyModule>();
 
-            //builder.RegisterModule<BusinessLogicDependencyModule>();
+            builder.RegisterModule<BusinessLogicDependencyModule>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(context => GlobalErrorHandler(context, env));
+            });
+
+            
             app.UseStatusCodePages();
 
             app.UseSwagger(c => { c.SerializeAsV2 = true; });
@@ -83,6 +103,84 @@ namespace KFU.CinemaOnline.API
             {
                 endpoints.MapControllers();
             });
+        }
+        
+        private static async Task GlobalErrorHandler(HttpContext context, IHostEnvironment env)
+        {
+            var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+
+            var commonError = exceptionHandlerPathFeature?.Error;
+            if (commonError == null)
+            {
+                return;
+            }
+
+
+            int statusCode;
+            string errorType;
+            string message;
+
+            switch (commonError)
+            {
+                case HttpOperationException httpError:
+                {
+                    switch (httpError.Response.StatusCode)
+                    {
+                        case HttpStatusCode.BadRequest:
+                            statusCode = (int) HttpStatusCode.BadRequest;
+                            errorType = HttpStatusCode.BadRequest.ToString();
+                            break;
+                        case HttpStatusCode.NotFound:
+                            statusCode = (int) HttpStatusCode.NotFound;
+                            errorType = HttpStatusCode.NotFound.ToString();
+                            break;
+                        case HttpStatusCode.Conflict:
+                            statusCode = (int) HttpStatusCode.Conflict;
+                            errorType = HttpStatusCode.Conflict.ToString();
+                            break;
+                        case HttpStatusCode.Forbidden:
+                            statusCode = (int) HttpStatusCode.Forbidden;
+                            errorType = HttpStatusCode.Forbidden.ToString();
+                            break;
+                        case HttpStatusCode.MethodNotAllowed:
+                            statusCode = (int) HttpStatusCode.MethodNotAllowed;
+                            errorType = HttpStatusCode.Forbidden.ToString();
+                            break;
+                        case HttpStatusCode.Unauthorized:
+                            statusCode = (int) HttpStatusCode.Unauthorized;
+                            errorType = HttpStatusCode.Unauthorized.ToString();
+                            break;
+                        default:
+                            statusCode = (int) HttpStatusCode.InternalServerError;
+                            errorType = HttpStatusCode.InternalServerError.ToString();
+                            break;
+                    }
+
+                    message = httpError.Response.Content;
+                    break;
+                }
+                case UnauthorizedAccessException _:
+                    statusCode = (int) HttpStatusCode.Unauthorized;
+                    errorType = HttpStatusCode.Unauthorized.ToString();
+                    message = commonError.Message;
+                    break;
+                default:
+                    statusCode = (int) HttpStatusCode.InternalServerError;
+                    errorType = HttpStatusCode.InternalServerError.ToString();
+                    message = env.IsDevelopment() ? commonError.ToString() : commonError.Message;
+                    break;
+            }
+
+            context.Response.StatusCode = statusCode;
+            context.Response.ContentType = "application/json";
+            var error = new ErrorModel
+            {
+                StatusCode = statusCode,
+                Error = errorType,
+                Message = message,
+                Time = DateTime.Now
+            };
+            await context.Response.WriteAsync(JsonConvert.SerializeObject(error)).ConfigureAwait(false);
         }
     }
 }
---
+++ KFU.CinemaOnline.Common/ErrorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace KFU.CinemaOnline.Common
+{
+    public class ErrorModel
+    {
+        [JsonProperty(PropertyName = "statusCode")]
+        public int StatusCode { get; set; }
+
+        [JsonProperty(PropertyName = "error")]
+        public string Error { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "time")]
+        public DateTime Time { get; set; }
+    }
+}
