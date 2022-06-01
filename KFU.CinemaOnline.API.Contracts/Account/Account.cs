91fc4599f44f920c02f4a46a2d5ce5fdebd693a3
pavel_gavrilichev <Pavel.Gavrilichev@waveaccess.ru>
2022-06-01 23:16:49 +0300

    add account crud

--- KFU.CinemaOnline.API.Contracts/Account/Account.cs
+++ KFU.CinemaOnline.API.Contracts/Account/Account.cs
@@ -7,6 +7,7 @@ namespace KFU.CinemaOnline.API.Contracts.Account
         public int Id { get; set; }
         public string Email { get; set; }
         public string Username { get; set; }
+        public DateTime CreatedAt { get; set; }
         public Role[] Roles { get; set; }
     }
 }
--- KFU.CinemaOnline.Core/Account/IAccountRepository.cs
+++ KFU.CinemaOnline.Core/Account/IAccountRepository.cs
@@ -1,9 +1,13 @@
-﻿namespace KFU.CinemaOnline.Core.Account
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KFU.CinemaOnline.Core.Account
 {
     public interface IAccountRepository : IEfRepository<AccountEntity>
     {
         AccountEntity GetByUsernameAndPassword(string username, string password);
         AccountEntity GetByUsername(string username);
         AccountEntity GetByEmail(string email);
+        Task<List<AccountEntity>> GetAccounts();
     }
 }
